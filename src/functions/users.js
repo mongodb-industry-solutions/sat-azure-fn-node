71ef3a35879d88f9a5aac40e117c34e966ddffc2
Antonio Membrides Espinosa <tonykssa@gmail.com>
2025-04-25 11:51:35 +0200

    fix the router

--- src/functions/users.js
+++ src/functions/users.js
@@ -4,8 +4,44 @@ const UserController = require('../controllers/users');
 
 const controller = new UserController(new UserService());
 
-app.http('GetUsers', {
-    methods: ['GET', 'POST'],
+app.http('UserMetadata', {
+    methods: ['OPTIONS'],
     authLevel: 'anonymous',
+    route: 'users',
+    handler: (request, context) => controller.getMetadata(request, context)
+});
+
+app.http('UserGetAll', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    route: 'users', 
     handler: (request, context) => controller.getAll(request, context)
+});
+
+app.http('UserInsert', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    route: 'users', 
+    handler: (request, context) => controller.create(request, context)
+});
+
+app.http('UserSelect', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    route: 'users/{id}',
+    handler: (request, context) => controller.getById(request, context)
+});
+
+app.http('UserUpdate', {
+    methods: ['PUT'],
+    authLevel: 'anonymous',
+    route: 'users/{id}', 
+    handler: (request, context) => controller.update(request, context)
+});
+
+app.http('UserDelete', {
+    methods: ['DELETE'],
+    authLevel: 'anonymous',
+    route: 'users/{id}', 
+    handler: (request, context) => controller.delete(request, context)
 });
