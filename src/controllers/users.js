94b5d239b0c02e1635cff4db2df7f2bbf9fd9ff8
Antonio Membrides Espinosa <tonykssa@gmail.com>
2025-04-25 11:51:26 +0200

    improve controller

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -4,20 +4,20 @@ class UserController {
         this.service = service;
     }
 
-    async getAll(request, context) {
+    /**
+     * Retrieves the service metadata
+     * @param {Request} request - The HTTP request.
+     * @param {InvocationContext} context - The HTTP response.
+     */
+    async getMetadata(request, context) {
         try {
             context?.log(`Http function processed request for url "${request?.url}"`);
-            const srvUser = this.service;
-            const conn = await srvUser.connect();
-
-            const name = request?.query.get('name') || await request?.text() || 'world';
-            const data = !conn ? `Hello, ${name}!` : await srvUser.getAll();
-
+            await this.service?.connect();
             return {
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({ data, metadata: srvUser.metadata, env: process.env })
+                body: JSON.stringify({ action: "getMetadata", metadata: this.service.metadata, env: process.env })
             };
         }
         catch (error) {
@@ -27,6 +27,172 @@ class UserController {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify({ error: error.message })
+            };
+        }
+    }
+
+    /**
+     * Creates a new user.
+     * @param {Request} request - The HTTP request.
+     * @param {InvocationContext} context - The HTTP response.
+     */
+    async create(request, context) {
+        try {
+            const user = await this.service.create(request.body);
+            return {
+                status: 201,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ action: "create", data: user })
+            };
+        }
+        catch (error) {
+            return {
+                status: 500,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ action: "create", error: error.message })
+            };
+        }
+    }
+
+    /**
+     * Retrieves all users.
+     * @param {Request} request - The HTTP request.
+     * @param {InvocationContext} context - The HTTP response.
+     */
+    async getAll(request, context) {
+        try {
+            context?.log(`Http function processed request for url "${request?.url}"`);
+            // const name = request?.query.get('name') || await request?.text() || 'world';
+            const data = await this.service.getAll() || [];
+            return {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ action: "getAll", data })
+            };
+        }
+        catch (error) {
+            return {
+                status: 500,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ action: "getAll", error: error.message })
+            };
+        }
+    }
+
+    /**
+     * Retrieves a user by their ID.
+     * @param {Request} request - The HTTP request.
+     * @param {InvocationContext} context - The HTTP response.
+     */
+    async getById(request, context) {
+        try {
+            const user = await this.service.getById(request.params.id);
+
+            if (!user) {
+                return {
+                    status: 404,
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ action: "getById", message: 'User not found' })
+                };
+            }
+
+            return {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ action: "getById", data: user })
+            };
+        }
+        catch (error) {
+            return {
+                status: 500,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ action: "getById", error: error.message })
+            };
+        }
+    }
+
+    /**
+     * Updates an existing user.
+     * @param {Request} request - The HTTP request.
+     * @param {InvocationContext} context - The HTTP response.
+     */
+    async update(request, context) {
+        try {
+            const user = await this.service.update(request.params.id, request.body);
+
+            if (!user) {
+                return {
+                    status: 404,
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ action: "update", message: 'User not found' })
+                };
+            }
+
+            return {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ action: "update", data: user })
+            };
+        }
+        catch (error) {
+            return {
+                status: 500,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ action: "update", error: error.message })
+            };
+        }
+    }
+
+    /**
+     * Deletes a user by ID.
+     * @param {Request} request - The HTTP request.
+     * @param {InvocationContext} context - The HTTP response.
+     */
+    async delete(request, context) {
+        try {
+            const user = await this.service.delete(request.params.id);
+
+            if (!user) {
+                return {
+                    status: 404,
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ action: "delete", message: 'User not found' })
+                };
+            }
+
+            return {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ action: "delete", data: user })
+            };
+        }
+        catch (error) {
+            return {
+                status: 500,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ action: "delete", error: error.message })
             };
         }
     }
